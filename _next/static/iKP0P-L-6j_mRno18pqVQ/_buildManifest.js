d9dc46e1785e44a6d8c9d9e2c5f398390c03d999
nkwtnb <nkwtnb@users.noreply.github.com>
2022-03-17 12:18:25 +0000

    deploy: c54d5d5844e8483d0fc8626bcc4140ec2d141846

---
+++ _next/static/iKP0P-L-6j_mRno18pqVQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/800-2d9351c92879d834.js","static/chunks/pages/index-3fa098342235a44c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
