48dc653c11a61dad03217de4bef0798f7aa53e5c
nkwtnb <n.watanabe0305@gmail.com>
2022-03-17 12:26:36 +0900

    build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const PREFIX = '/utility-components';
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: PREFIX,
+  basePath: PREFIX,
 }
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  env: {
+    basePath: PREFIX,
+  },
+}
